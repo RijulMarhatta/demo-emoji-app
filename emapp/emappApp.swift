cee106413e79bdab9cd898cf67e5538ba2ff185a
RijulMarhatta <rijulmarhatta.gmail.com>
2024-11-03 21:58:11 +0530

    Initial Commit

---
+++ emapp/emappApp.swift
@@ -0,0 +1,17 @@
+//
+//  emappApp.swift
+//  emapp
+//
+//  Created by Rijul Marhatta  on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct emappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ emappTests/emappTests.swift
@@ -0,0 +1,17 @@
+//
+//  emappTests.swift
+//  emappTests
+//
+//  Created by Rijul Marhatta  on 3/11/24.
+//
+
+import Testing
+@testable import emapp
+
+struct emappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
